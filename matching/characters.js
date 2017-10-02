283933757ccb6e267309a3b278b00b20edff4492
Melanie West <dhmofan@gmail.com>
2017-10-01 23:17:34 -0400

    Begin to fill in index file with character images. The javascript file 'match' has been commented with pseudocode. The objects that contain character values and questions have been initialized.  Nothing is yet in the questions file

---
+++ matching/characters.js
@@ -0,0 +1,42 @@
+var characters =[
+    {
+        name: "Roo",
+        xvalue: -1,
+        yvalue: 1
+    },
+    {
+        name: "Pooh Bear",
+        xvalue: 0,
+        yvalue: 2
+    },
+    {
+        name: "Piglet",
+        xvalue: 1,
+        yvalue: 1
+    },
+    {
+        name: "Tigger",
+        xvalue: -2,
+        yvalue: 0
+    },
+    {
+        name: "Eeyore",
+        xvalue: 2,
+        yvalue: 0
+    },
+    {
+        name: "Kanga",
+        xvalue: -1,
+        yvalue: -1
+    },
+    {
+        name: "Rabbit",
+        xvalue: 0,
+        yvalue: -2
+    },
+    {
+        name: "Owl",
+        xvalue: 1,
+        yvalue: -1
+    }
+];
---
+++ matching/match.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    //read in data and evaluate matches by the 
+    // following process:
+    //part 0:  define characters by grid position
+
+    //part 1:  define question and answer objects
+    // (use a separate javascript file for this)
+
+    //part 2:  loop through the questions and quantify
+    // the answers in (x,y)format
+    for (var k=0; k<questions.length; k++){
+
+    }
+
+    // part 3:  average the x & y values
+
+    function average(){
+
+    }
+
+    // part 4:  use the distance formula from avg to each
+    // character.  then find the minimum value. 
+
+    function distance(){
+
+    }
+})

9bf546e8eef1ba0bbf634e79eaade5e7886fe8b0
Melanie West <dhmofan@gmail.com>
2017-10-03 18:48:07 -0400

    Change index file to include question array, since it's an array and not an object, and it's front end.  Files that need to be removed from this repo:  data/questions.js, public/images (dir), and public/style.css.  None of these are used anymore

--- matching/data/questions.js
+++ matching/data/questions.js
@@ -1,11 +1,3 @@
-var questionsArray =
-["I like hard work",
-"I prefer sleeping and eating",
-"I bring the party with me",
-"I am very cautious",
-"I think life is very hard",
-"I am very curious",
-"I like to think before I act",
-"I think it's important to plan ahead"]
 
-module.exports= questionsArray;
+
+
